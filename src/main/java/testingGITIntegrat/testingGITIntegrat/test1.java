8a3e3dba8ad53824f55a2715a50404b9fdd3d5cc
mani <rmanimala5@gmail.com>
2024-12-25 16:20:27 +0530

    first commit

---
+++ src/main/java/testingGITIntegrat/testingGITIntegrat/test1.java
@@ -0,0 +1,16 @@
+package testingGITIntegrat.testingGITIntegrat;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int i;
+		int a =10;
+		int b=20;
+		
+		System.out.println(a+b);
+		
+		int c=20,d=30;
+	}
+
+}
---
+++ src/main/java/testingGITIntegrat/testingGITIntegrat/test3.java
@@ -0,0 +1,16 @@
+package testingGITIntegrat.testingGITIntegrat;
+
+public class test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a =10;
+		int b=20;
+		
+		System.out.println(a+b);
+		
+		int c=20,d=30;
+	}
+
+}
